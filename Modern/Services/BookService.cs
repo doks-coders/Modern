474e24859edfde8788e7dacdf158a152db8d61ad
doks-coders <guonnie@gmail.com>
2025-10-03 15:54:03 +0100

    testing

--- Modern/Services/BookService.cs
+++ Modern/Services/BookService.cs
@@ -40,6 +40,7 @@ namespace Modern.Services
 				return null;
 			return BookResponse.MapFromBook(item);
 		}
+		
 		public async Task<IEnumerable<BookResponse>> GetAll()
 		{
 			var items = await repository.GetAll();
